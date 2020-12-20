fdbd730aa6ed5a994f5b6fa1d3765c161c47650d
SilverPineValley <whddls9632@naver.com>
2020-12-20 19:59:35 +0900

    xlsx import -> vendor 문제해결 필요

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/tealeg/xlsx"
 )
 
 func main() {
@@ -11,18 +13,19 @@ func main() {
 		fmt.Println("./main.exe (J2파일) (CJ파일) 로 입력해주세요.")
 	}
 
-	// js :=
+	j2 := args[0]
+	cj := args[1]
 
-	// // open an existing file
-	// wb, err := xlsx.OpenFile("../samplefile.xlsx")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// // wb now contains a reference to the workbook
-	// // show all the sheets in the workbook
-	// fmt.Println("Sheets in this file:")
-	// for i, sh := range wb.Sheets {
-	// 	fmt.Println(i, sh.Name)
-	// }
-	// fmt.Println("----")
+	// open an existing file
+	wb, err := xlsx.OpenFile("../samplefile.xlsx")
+	if err != nil {
+		panic(err)
+	}
+	// wb now contains a reference to the workbook
+	// show all the sheets in the workbook
+	fmt.Println("Sheets in this file:")
+	for i, sh := range wb.Sheets {
+		fmt.Println(i, sh.Name)
+	}
+	fmt.Println("----")
 }
