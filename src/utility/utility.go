84d79ad2eb4cd7afdc8c951e21ca8c6fc3096d60
JonginPark <whddls9632@naver.com>
2021-03-20 12:47:30 +0900

    파싱, 출력 함수 Utility 내로 분산 (#24)

--- src/utility/utility.go
+++ src/utility/utility.go
@@ -1,6 +1,72 @@
 package utility
 
-import "sort"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+
+	"js.comp.dispatching/src/models"
+)
+
+func GetParameters() (j2SheetName, cjSheetName, gansunSheetName, resultFileName, resultType, companyFilter, errCode string) {
+	fmt.Print("J2 파일 시트명(Default: sheet1): ")
+	j2Buf := bufio.NewScanner(os.Stdin)
+	j2Buf.Scan()
+	j2SheetName = j2Buf.Text()
+
+	if CjContain {
+		fmt.Print("CJ 파일 시트명(Default: sheet1): ")
+		cjBuf := bufio.NewScanner(os.Stdin)
+		cjBuf.Scan()
+		cjSheetName = cjBuf.Text()
+	}
+
+	if GansunContain {
+		fmt.Print("간선 파일 시트명(Default: sheet1): ")
+		gansunBuf := bufio.NewScanner(os.Stdin)
+		gansunBuf.Scan()
+		gansunSheetName = gansunBuf.Text()
+	}
+
+	fmt.Print("결과 파일명(Default: result.csv): ")
+	resBuf := bufio.NewScanner(os.Stdin)
+	resBuf.Scan()
+	resultFileName = resBuf.Text()
+
+	fmt.Print("노선 필터(TOML파일 target): ")
+	fmt.Scanln(&resultType)
+
+	fmt.Print("업체 필터(TOML파일 company): ")
+	fmt.Scanln(&companyFilter)
+
+	if j2SheetName == "" {
+		j2SheetName = "sheet1"
+	}
+
+	if cjSheetName == "" {
+		cjSheetName = "sheet1"
+	}
+
+	if gansunSheetName == "" {
+		gansunSheetName = "sheet1"
+	}
+
+	if resultFileName == "" {
+		resultFileName = "result.csv"
+	}
+
+	if _, exists := ConfigFile.Target[resultType]; !exists {
+		resultType = "1"
+	}
+
+	if _, exists := ConfigFile.Company[companyFilter]; !exists {
+		errCode = models.InvalidCompany
+	}
+
+	return
+}
 
 // Contains is check the substr in array s.
 func Contains(s []string, substr string) bool {
@@ -81,4 +147,23 @@ func ArraySum(arr []int) (sum int) {
 	}
 
 	return
+}
+
+// 가져오는 데이터가 맞는지 조건 확인
+func checkGetData(source, dest, parseType string) bool {
+	for _, target := range ConfigFile.Target[parseType] {
+		if strings.Contains(source, target) || strings.Contains(dest, target) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Layover Check
+func checkLayover(key string) string {
+	if value, exists := ConfigFile.Direct[key]; exists {
+		return value
+	}
+	return key
 }
---
+++ src/utility/write.go
@@ -0,0 +1,88 @@
+package utility
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+
+	"golang.org/x/text/encoding/korean"
+	"golang.org/x/text/transform"
+	"js.comp.dispatching/src/models"
+)
+
+var CjContain bool = false
+var GansunContain bool = false
+
+func WriteXlsxData(comData []models.CompData) {
+	// file := xlsx.NewFile()
+}
+
+func WriteCSVData(resultFileName string, comData []models.CompData) {
+	// Comp 파일 생성
+	resFile, err := os.Create("./" + resultFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	// Comp csv writer 생성
+	w := bufio.NewWriter(resFile)
+	resWr := transform.NewWriter(w, korean.EUCKR.NewEncoder())
+
+	// Comp 내용 쓰기
+	if CjContain && GansunContain {
+		resWr.Write([]byte("날짜, 차량번호, 출발, 도착, 간선여부, J2, CJ, Gansun, 추가운임구분, 추가운임, 추가운임구분3, 추가운임3, 다회전기준요율, 청구, " + ConfigFile.Cj.TotalFee + ", SJ비고, CJ비고, 완료단계\n"))
+	} else if CjContain && !GansunContain {
+		resWr.Write([]byte("날짜, 차량번호, 출발, 도착, 간선여부, J2, CJ, 추가운임구분, 추가운임, 추가운임구분3, 추가운임3, 다회전기준요율, 청구, " + ConfigFile.Cj.TotalFee + ", SJ비고, CJ비고, 완료단계\n"))
+	} else if !CjContain && GansunContain {
+		resWr.Write([]byte("날짜, 차량번호, 출발, 도착, 간선여부, J2, Gansun, 추가운임구분, 추가운임, 추가운임구분3, 추가운임3, 다회전기준요율, 청구, " + ConfigFile.Gansun.TotalFee + ", SJ비고, CJ비고, 완료단계\n"))
+	}
+
+	for _, value := range comData {
+		gansun := ""
+		if value.IsGansun && value.IsGansunOneway {
+			gansun = "간선편도"
+		} else if value.IsGansun && !value.IsGansunOneway {
+			gansun = "고정간선"
+		}
+
+		// 자체 No, 날짜, 차량번호, 출발, 도착, 간선
+		each := value.Date[0:10] + "," + value.LicensePlate + "," + value.Source + "," + value.Destination + "," + gansun
+
+		// No 숫자, 비고
+		if CjContain && GansunContain {
+			if value.J2 && value.CJ {
+				each = each + "," + strconv.Itoa(len(value.J2No)) + "," + strconv.Itoa(len(value.CJNo)) + ", 0"
+			} else if value.J2 && value.Gansun {
+				each = each + "," + strconv.Itoa(len(value.J2No)) + ", 0, " + strconv.Itoa(len(value.GansunNo))
+			} else if value.J2 && !value.CJ && !value.Gansun {
+				each = each + "," + strconv.Itoa(len(value.J2No)) + ", 0, 0"
+			} else if !value.J2 && value.CJ && !value.Gansun {
+				each = each + ",0," + strconv.Itoa(len(value.CJNo)) + ", 0"
+			} else if !value.J2 && !value.CJ && value.Gansun {
+				each = each + ",0, 0," + strconv.Itoa(len(value.GansunNo))
+			}
+		} else if CjContain && !GansunContain {
+			if value.J2 && !value.CJ {
+				each = each + "," + strconv.Itoa(len(value.J2No)) + ", 0"
+			} else if !value.J2 && value.CJ {
+				each = each + ",0," + strconv.Itoa(len(value.CJNo))
+			} else {
+				each = each + "," + strconv.Itoa(len(value.J2No)) + "," + strconv.Itoa(len(value.CJNo))
+			}
+		} else if !CjContain && GansunContain {
+			if value.J2 && !value.Gansun {
+				each = each + "," + strconv.Itoa(len(value.J2No)) + ", 0"
+			} else if !value.J2 && value.Gansun {
+				each = each + ",0," + strconv.Itoa(len(value.GansunNo))
+			} else {
+				each = each + "," + strconv.Itoa(len(value.J2No)) + "," + strconv.Itoa(len(value.GansunNo))
+			}
+		}
+
+		each = each + "," + GetArrayData(value.DetourFeeType, " / ") + "," + strconv.Itoa(value.DetourFee) + "," + GetArrayData(value.DetourFeeType3, " / ") + "," + strconv.Itoa(value.DetourFee3) + "," + GetArrayData(value.MultiTourPercent, " / ") + "," + strconv.Itoa(value.FirstTotalFee) + "," + strconv.Itoa(value.SecondTotalFee) + "," + GetArrayData(value.J2Reference, " / ") + "," + GetArrayData(value.CJReference, " / ") + "," + GetStage(value.Stage) + "\n"
+		resWr.Write([]byte(each))
+		w.Flush()
+	}
+
+	resWr.Close()
+}
